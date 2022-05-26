45592040dc1bcd8484dff90037fd932d276e64ec
unknown <j.currier224@gmail.com>
2022-05-25 21:29:52 -0600

    scroll feature started

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 
 tutorial @ https://www.youtube.com/watch?v=YWN8GcmJ-jA
-11:39
+11:40
+16:14
+24:30
--- level.py
+++ level.py
@@ -1,10 +1,26 @@
 import pygame
 from tiles import Tile
+from settings import tile_size
 
 class Level:
     def __init__(self, level_data, surface):
         self.display_surface = surface
-        self.level_data = level_data
+        self.setup_level(level_data)
+     
+     
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
         
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                if cell == 'X':
+                    x = col_index*tile_size #left right position
+                    y = row_index*tile_size #up down position
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+                    
+    
+       
     def run(self):
-        pass
+        self.tiles.update(1)
+        self.tiles.draw(self.display_surface)
