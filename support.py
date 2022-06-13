6337a0320bf13d4cba496970947c5a48c6ac235a
unknown <j.currier224@gmail.com>
2022-06-12 18:14:49 -0600

    import images func

--- support.py
+++ support.py
@@ -1,11 +1,15 @@
 from os import walk #used to import folders using a file system (walk)
 #walk returns the directory path, directory names, filenames
+import pygame
 
 def import_folder(path):
-    print("here")
-
-    for myinfo in walk(path):
-        print(list(myinfo))
-        print("hi")
+    surface_list = []
+    for 1, 2, img_file in walk(path):
+        for image in img_file:
+            ''' import images, put on surface, place the surface in a list, return list'''
+            full_path = path + "/" + image
+            my_surface = pygame.image.load(full_path).convert_alpha()
+            #use convert alpha on png files
+            surface_list.append(my_surface)
+    return surface_list
         
-import_folder('graphics/character/run')
