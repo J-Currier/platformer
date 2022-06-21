ae54afc31b44ed58390e552422cc58822a8be4d7
unknown <j.currier224@gmail.com>
2022-06-21 15:18:20 -0600

    char image flip

--- README.md
+++ README.md
@@ -15,4 +15,6 @@ tutorial @ https://www.youtube.com/watch?v=YWN8GcmJ-jA *2:19:23
 1:09:10
 1:11:47 -> 2 hours file path// vs backslash
 ~1:10 taking image and writing to surface reveiw for ground images
+1:16:45
+1:24:24
 
--- player.py
+++ player.py
@@ -16,6 +16,10 @@ class Player(pygame.sprite.Sprite):
         self.speed = 8
         self.gravity = 0.8
         self.jump_speed = -16 #neagtive b/c player jumps UP
+        
+        #player status
+        self.status = 'idle'
+        self.facing_right = True 
 
     def import_character_assests(self):
         my_path = path.join("graphics", "character")
@@ -31,26 +35,46 @@ class Player(pygame.sprite.Sprite):
             self.animations[animation] = import_folder(my_full_path)
     
     def animate(self): 
-        animation = self.animations['run']
+        animation = self.animations[self.status]
         
         #loop over frame index
         self.frame_index += self.animation_speed
         if self.frame_index >= len(animation):
             self.frame_index = 0
         
-        self.image = animation[int(self.frame_index)]
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else: 
+            flipped_image = pygame.transform.flip(image, True, False) #(surface, flip horiz., flip vert.)
+            self.image = flipped_image
+        
     def get_input(self):
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
         
         if keys[pygame.K_SPACE]:
             self.jump()
+    
+    def get_status(self):
+        if self.direction.y < 0:
+            self.status = 'jump'
+        elif self.direction.y > 1:
+            self.status = 'fall'
+        else:
+            if self.direction.x != 0:
+                self.status = 'run'
+            else:
+                self.status = 'idle' 
+            
             
     def apply_gravity(self):
         self.direction.y += self.gravity #makes gravity increase every frame
@@ -61,4 +85,5 @@ class Player(pygame.sprite.Sprite):
     
     def update(self):
         self.get_input()
+        self.get_status()
         self.animate()
