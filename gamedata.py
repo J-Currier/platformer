a69deccef3bbfbf321a940e4ef4ddd6f25a07116
unknown <j.currier224@gmail.com>
2022-12-15 14:08:59 -0700

    updated address format

--- gamedata.py
+++ gamedata.py
@@ -1,13 +1,13 @@
 level_0 = {
     'terrain': ['2 - Level', 'levels', '0', 'level_0._terrain.csv'],
-    'bg_palms': '2 - Level\levels\0\level_0._bg_palms.csv',
-    'coins': '2 - Level\levels\0\level_0._coins.csv',
-    'fg_palms': '2 - Level\levels\0\level_0._fg_palms.csv',
-    'crates': '2 - Level\levels\0\level_0._crates.csv',
-    'enemies': '2 - Level\levels\0\level_0._enemies.csv',
-    'contraints': '2 - Level\levels\0\level_0._contraints.csv',
-    'player': '2 - Level\levels\0\level_0._player.csv',
-    'grass': '2 - Level\levels\0\level_0._grass.csv'
+    'bg_palms': ['2 - Level', 'levels', '0', 'level_0._bg_palms.csv'],
+    'coins': ['2 - Level', 'levels', '0', 'level_0._coins.csv'],
+    'fg_palms': ['2 - Level', 'levels', '0', 'level_0._fg_palms.csv'],
+    'crates': ['2 - Level', 'levels', '0', 'level_0._crates.csv'],
+    'enemies': ['2 - Level', 'levels', '0', 'level_0._enemies.csv'],
+    'contraints': ['2 - Level', 'levels', '0', 'level_0._contraints.csv'],
+    'player': ['2 - Level', 'levels', '0', 'level_0._player.csv'],
+    'grass': ['2 - Level', 'levels', '0', 'level_0._grass.csv']
 }
     
             
